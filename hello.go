c988c16f92329f7e83f4171e28a8534380fc531d
Breno de Oliveira Veroneze <oliveirabreno.veroneze@gmail.com>
2024-12-22 15:14:00 -0300

    2024-12-22 - final-curso-golang

--- hello.go
+++ hello.go
@@ -1,9 +1,147 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
-	fmt.Println("Olá Mundo, Breno!")
-	fmt.Println("Meu primeiro programa em GO")
+	exibeIntroducao()
+	for {
+		exibeMenu()
+		comando := leComando()
+		// if comando == 1 {
+		// 	fmt.Println("Monitorando...")
+		// } else if comando == 2 {
+		// 	fmt.Println("Exibindo Logs...")
+		// } else if comando == 0 {
+		// 	fmt.Println("Saindo do programa")
+		// } else {
+		// 	fmt.Println("Não conheço este comando")
+		// }
 
+		switch comando {
+		case 1:
+			IniciarMonitoramento()
+		case 2:
+			imprimeLogs()
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+
+}
+
+func exibeIntroducao() {
+	nome := "Breno"
+	versao := 1.1
+	fmt.Println("Olá, Sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func leComando() int {
+	comandoLido := 0
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func IniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := leSitesDoArquivo()
+
+	//sites := []string{"https://www.alura.com.br", "https://httpstat.us/502", "https://httpstat.us/504"}
+
+	fmt.Println(sites)
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+	if err != nil {
+		fmt.Print("Ocorreu um erro:", err)
+	}
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
+	}
+}
+
+func leSitesDoArquivo() []string {
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+	//arquivo, err := os.ReadFile("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+	fmt.Println(sites)
+
+	arquivo.Close()
+	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + site + "- online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	fmt.Println("Exibindo Logs...")
+	arquivo, err := os.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
+	}
+
+	fmt.Println(string(arquivo))
 }
